8eb9506eadf7a1df49863456ccd2fb93077921fd
icancode20 <icancode20@gmail.com>
2022-07-25 22:11:32 -0700

    Initial Commit

---
+++ Eyes/EyesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EyesApp.swift
+//  Eyes
+//
+//  Created by HomeMac on 7/25/22.
+//
+
+import SwiftUI
+
+@main
+struct EyesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
